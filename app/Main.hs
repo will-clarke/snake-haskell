5693ed40c2adac3986fea65aa703e742676fc8a7
William Clarke <wmmclarke@gmail.com>
2019-01-15 07:48:46 +0000

    Actuall display something! Just characters

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,28 @@
 module Main where
 
-import Lib
-import Brick
+import           Brick ((<+>), (<=>), Widget, emptyWidget, str, simpleMain, withBorderStyle)
+import           Brick.Widgets.Border (borderWithLabel)
+import           Brick.Widgets.Border.Style (unicodeRounded)
+import           Brick.Widgets.Center (center)
+
+firstLine :: Widget()
+firstLine = foldr (<+>) emptyWidget [str "h", str "e", str "y"]
+
+secondLine :: Widget()
+secondLine = foldr (<+>) emptyWidget [str "y", str "o"]
+
+thirdLine :: Widget()
+thirdLine = foldr (<+>) emptyWidget [str "h", str "a", str " ", str "h", str "a"]
+
+stackedLines :: Widget()
+stackedLines = foldr (<=>) emptyWidget [firstLine, secondLine, thirdLine]
+
+ui :: Widget ()
+ui =
+  withBorderStyle unicodeRounded $
+  borderWithLabel
+    (str "Hello!")
+    (center stackedLines)
 
 main :: IO ()
-main = someFunc
+main = simpleMain ui

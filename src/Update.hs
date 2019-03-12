842d6284958d0ac7e929206f00a0cb8006e62349
William Clarke <wmmclarke@gmail.com>
2019-03-12 08:09:04 +0000

    Replaying is now a feature

--- src/Update.hs
+++ src/Update.hs
@@ -65,13 +65,21 @@ tickGame game = do
 
 handleEvent :: M.State -> B.BrickEvent M.Name M.Tick -> B.EventM M.Name (B.Next M.State)
 handleEvent (M.Playing game) event = handlePlaying game event
-handleEvent (M.Replaying game) _ = handleReplay game
+handleEvent (M.Replaying game) event = handleReplay game event
 handleEvent (M.StartScreen options tvar) event = handleStartScreen options tvar event
 handleEvent (M.Paused game) event = handlePaused game event
 handleEvent (M.GameOver game) event = handleGameOver game event
 
-handleReplay :: M.Game -> B.EventM M.Name (B.Next M.State)
-handleReplay game | null (M.getPreviousGames game) = B.continue $ M.GameOver game
+handleReplay :: M.Game -> B.BrickEvent M.Name M.Tick -> B.EventM M.Name (B.Next M.State)
+-- handleReplay game (B.AppEvent M.Tick) = B.continue $ M.Replaying $ gamePopped game
+handleReplay game _ | null (M.getPreviousGames game) = B.continue $ M.GameOver game
+handleReplay game _ = B.continue $ M.Replaying $ gamePopped game
+
+gamePopped :: M.Game -> M.Game
+gamePopped game = let
+  games = M.getPreviousGames game
+  (_oldGame:newGame:newGames) = games
+  in newGame { M.getPreviousGames = newGames }
 
 handlePlaying :: M.Game -> B.BrickEvent M.Name M.Tick -> B.EventM M.Name (B.Next M.State)
 handlePlaying game (B.VtyEvent (V.EvKey (V.KChar 'q') [])) = B.halt $ M.Playing game
@@ -99,6 +107,11 @@ handleStartScreen options tvar _ = B.continue $ M.Playing (Game.initialGame opti
 
 handleGameOver :: M.Game -> B.BrickEvent M.Name M.Tick -> B.EventM M.Name (B.Next M.State)
 handleGameOver game (B.AppEvent M.Tick) = B.continue $ M.GameOver game
+handleGameOver game (B.VtyEvent (V.EvKey (V.KChar 'r') [])) =
+  B.continue $ M.Replaying reversedGames
+  where
+    previousGames = M.getPreviousGames game
+    reversedGames = game { M.getPreviousGames = reverse previousGames}
 handleGameOver game _ = B.halt $ M.GameOver game
 
 handlePaused :: M.Game -> B.BrickEvent M.Name M.Tick -> B.EventM M.Name (B.Next M.State)
